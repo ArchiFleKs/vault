8bddb62e6ff3cd711fff40cfb37b7e42000499c3
Mike Palmiotto <mike.palmiotto@hashicorp.com>
2022-11-21 11:40:31 -0500

    Move PendingRemovalMountsAllowed into Core/CoreConfig

--- vault/core.go
+++ vault/core.go
@@ -645,6 +645,8 @@ type Core struct {
 	effectiveSDKVersion string
 
 	rollbackPeriod time.Duration
+
+	pendingRemovalMountsAllowed bool
 }
 
 func (c *Core) HAState() consts.HAState {
@@ -779,6 +781,8 @@ type CoreConfig struct {
 	EffectiveSDKVersion string
 
 	RollbackPeriod time.Duration
+
+	PendingRemovalMountsAllowed bool
 }
 
 // GetServiceRegistration returns the config's ServiceRegistration, or nil if it does
@@ -932,6 +936,7 @@ func CreateCore(conf *CoreConfig) (*Core, error) {
 		disableSSCTokens:               conf.DisableSSCTokens,
 		effectiveSDKVersion:            effectiveSDKVersion,
 		userFailedLoginInfo:            make(map[FailedLoginUser]*FailedLoginInfo),
+		pendingRemovalMountsAllowed:    conf.PendingRemovalMountsAllowed,
 	}
 
 	c.standbyStopCh.Store(make(chan struct{}))
